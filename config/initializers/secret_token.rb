4eb0d2c4882f417d45855207b793fb90eaff7587
Ben Johnson <bjohnson@binarylogic.com>
2017-05-19 16:17:41 -0500

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyRails32ExampleApp::Application.config.secret_token = '45dc0207ba497321643b4ed9d1660507b97a1ee3a91ed189104fc3157668fa8c7bc9bf6f7291e6f6dae76396665167989232b183afd25d0224e0c0e12a665bab'
